eba85c77041a5d9b32f098a8344c2b79dddc7d13
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-02-29 16:23:07 -0600

    [.] Update App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./screens/HomeScreen";
+import Countries from "./screens/Countries";
+import NotFound from "./components/NotFound";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App(){
+  return(
+    <BrowserRouter>
+      <>
+       <Nav/>
+       <Routes>
+          <Route index path="/" element={<Home/>}></Route>
+          <Route path="/countries" element={<Countries/>}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+       </Routes>
+      </>
+    </BrowserRouter>
   );
 }
-
-export default App;
