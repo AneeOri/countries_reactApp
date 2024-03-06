22a35d61857f0a41fa5b98dbc37fb1a1bec920b6
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-03-06 10:55:07 -0600

    [+] Create Country Page

---
+++ src/screens/CountryPage.js
@@ -0,0 +1,44 @@
+import { LinearProgress } from "@mui/material";
+import { useEffect, useReducer } from "react";
+import { useParams } from "react-router-dom";
+import { searchByCountryName } from "../utils/countries";
+import { countriesReducer, initialState } from "../helper/reducer";
+import { ResponseActionTypes } from "../helper/action";
+
+export default function CountryPage(){
+    const {countryId} = useParams();
+    const [state, dispatch] = useReducer(countriesReducer, initialState);
+    console.log(countryId, 'countryId');
+
+    useEffect(() => {
+        dispatch({type: ResponseActionTypes.started}); 
+
+        searchByCountryName(countryId).then((data) => {
+            dispatch({type:ResponseActionTypes.success, payload:data});
+        })
+        .catch((err) => {
+            dispatch({type:ResponseActionTypes.failure, payload: err.message});
+        });
+    },[countryId]); 
+
+    if(state.isLoading){
+        return <LinearProgress/>;
+    }
+
+    if(state.error){
+        return <p className="error">{state.error}</p>
+    }
+
+    const countryInfo = state?.countries[0] || {};
+
+    return(
+        <div>
+            <h1> Region: {countryInfo?.region}</h1>
+            <h2> Country: {countryInfo?.name?.common}</h2>
+            <h3> Capital: {countryInfo?.capital}</h3>
+            <h3> Official name: {countryInfo?.name?.official}</h3>
+            <h3> Population: {countryInfo?.population}</h3>
+            <img src={countryInfo?.flags?.png} alt={countryInfo?.flags?.alt} width="70"/>
+        </div>
+    );
+}
