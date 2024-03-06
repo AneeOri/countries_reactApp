d170fa9d1390b73a3a7cea2e35d1df3226a85f87
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-03-06 12:12:02 -0600

    [+] Update App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/Nav";
 import Home from "./screens/HomeScreen";
 import Countries from "./screens/Countries";
 import NotFound from "./components/NotFound";
+import CountryPage from "./screens/CountryPage";
 
 export default function App(){
   return(
@@ -12,6 +13,7 @@ export default function App(){
        <Routes>
           <Route index path="/" element={<Home/>}></Route>
           <Route path="/countries" element={<Countries/>}></Route>
+          <Route path="/countries/:countryId" element={<CountryPage/>}></Route>
           <Route path="*" element={<NotFound />}></Route>
        </Routes>
       </>
