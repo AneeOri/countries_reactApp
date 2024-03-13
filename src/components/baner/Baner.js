37de810a2228880d54cc4a51e640843845f2e5b0
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-03-13 11:33:41 -0600

    [.] Update Baner Component

--- src/components/baner/Baner.js
+++ src/components/baner/Baner.js
@@ -1,1 +1,64 @@
 import { Container, makeStyles, Typography } from "@mui/material";
+import Carousel from "./Carousel";
+import { useState } from "react";
+
+const useStyles = makeStyles((theme) => ({
+  banner:{
+    backgroungImage: "url(./banner2.jpg)",
+  },
+  bannerContent:{
+    height:400,
+    diaplay:"flex",
+    flexDirection: "column",
+    paddingTop:25,
+    justifyContent: "space-around",
+  },
+  tagline:{
+    display:"flex",
+    height:"40%",
+    flexDirection:"column",
+    justifyContext:"center",
+    textAlign:"center",
+  },
+  carousel:{
+    height: "50%",
+    display:"flex",
+    alignItems:"center",
+  },
+}));
+
+function Banner () {
+    const clases = useStyles();
+
+    return(
+        <div className={clases.banner}>
+            <Container className={clases.bannerContent}>
+              <div className={clases.tagline}>
+                <Typography
+                   variante="h2"
+                   style={{
+                    fontWeight: "bold",
+                    marginBottom:15,
+                    fontFamily:"Monserrat"
+                   }}
+                >
+                   Crypto Hunter
+                </Typography>
+                <Typography
+                  variant="subtitle2"
+                  style={{
+                     color: "darkgray",
+                     textTransform:"capitalize",
+                     fontFamily:"Monserrat"
+                  }}
+                >
+                   Get all the Info regarding the Crypto Currency
+                </Typography>
+              </div>
+               <Carousel/>
+            </Container>
+        </div>
+    );
+}
+
+export default Banner;
