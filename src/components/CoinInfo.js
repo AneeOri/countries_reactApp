96dd949ef22a474b03d6f93a2df2002b78086ab0
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-03-13 11:58:31 -0600

    [.] Update CoinInfo Component
    CoinInfo Styles

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -8,3 +8,35 @@ import {
     makesStyles,
     ThemeProvider,
 } from "@mui/material";
+import SelectButton from "./SelectButton";
+import {chartDays} from "../config/data";
+import {CryptoState} from "../context/CryptoContext";
+
+const CoinInfo = ({coin}) => {
+  const [historicData, setHistoricalData] = useState();
+  const [days, setDays] = useState(1);
+  const {currency} = CryptoState();
+  const [flag, setFlag] = useState(false);
+
+  const useStyles = makesStyles((theme) => ({
+    container: {
+        width: "75%",
+        display: "flex",
+        flexDirection:"column",
+        alignItems:"center",
+        justifyContent: "center",
+        marginTop: 25,
+        padding:40,
+        [theme.breakpoints.down("md")]: {
+            width:"100%",
+            marginTop:0,
+            padding:20,
+            paddingTop:0,
+        },
+    },
+  }));
+
+  const clases = useStyles();
+
+  
+}
