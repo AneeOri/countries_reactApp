bd8ca63634b176faba77d5337edae21ac08ed694
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-03-13 10:21:17 -0600

    [.] Update Carousel Baner Component

--- src/components/baner/Carousel.js
+++ src/components/baner/Carousel.js
@@ -5,3 +5,95 @@ import AliceCarousel from "react-alice-carousel";
 import { Link } from "react-router-dom";
 import {TrendingCoins} from "../../config/api";
 import { CryptoState } from "../../context/CryptoContext";
+import { numberWithCommas } from "../CoinsTable";
+
+const Carousel = () => {
+    const [trending, setTrending] = useState([]);
+    const {currency, symbol} = CryptoState();
+
+    const fetchTrendingCoins = async () => {
+        const {data} = await axios.get(TrendingCoins(currency));
+
+        console.log(data);
+        setTrending(data);
+    };
+
+    useEffect(() => {
+        fetchTrendingCoins();
+    },[currency]);
+
+    const useStyles = makeStyles((theme) => ({
+        carousel:{
+            height: "50%",
+            display:"flex",
+            alignItems:"center",
+        },
+        carouselItem:{
+            display:"flex",
+            flexDirection: "column",
+            alignItems:"center",
+            cursor:"pointer",
+            textTransform:"uppercase",
+            color:"white",
+        },
+    }));
+
+    const clases = useStyles();
+
+    const items = trending.map((coin) => {
+        let profit = coin?.price_change_percentaje_24h >= 0;
+        return(
+            <Link className={clases.carouselItem} to={`/coins/${coin.id}`}>
+                <img
+                  src={coin?.image}
+                  alt={coin.name}
+                  height="80"
+                  style={{marginBottom:10}}
+                />
+                <span>
+                    {coin?.symbol}
+                    &nbsp;
+                    <span
+                      style={{
+                        color:profit > 0 ? "rgb(14,203,129)" : "red",
+                        fontWeight:500,
+                      }}
+                    >
+                     {profit && "+"}
+                     {coin?.price_change_percentaje_24h?.toFixed(2)}%
+                    </span>
+                </span>
+                <span style={{fontSize:22, fontWeight:500}}>
+                  {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+                </span>
+            </Link>
+        );
+    });
+
+    const responsive = {
+        0:{
+            items:2,
+        },
+        512:{
+            items:4,
+        },
+    };
+
+    return(
+        <div className={clases.carousel}>
+          <AliceCarousel
+            mouseTracking
+            infinite
+            autoPlayInterval={1000}
+            animationDuration={1500}
+            disableDotsControls
+            disableButtonsControls
+            responsive={responsive}
+            items={items}
+            autoPlay
+          />
+        </div>
+    );
+};
+
+export default Carousel;
