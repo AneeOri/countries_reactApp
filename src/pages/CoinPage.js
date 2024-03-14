7409bf099294305549f0b0d8a368e323defbffbb
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-03-14 16:04:17 -0600

    [.] Update Coin Page

--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -76,5 +76,86 @@ const CoinPage = () => {
 
 const clases = useStyles();   
 
+if(!coin) return <LinearProgress style={{backgroundColor: "gold"}}/>
 
-}
+return(
+    <div className={clases.container}>
+      <div className={clases.sidebar}>
+        <img
+           src={coin?.image.large}
+           alt={coin?.name}
+           height="200"
+           style={{marginBottom:20}}
+        />
+        <Typography variant="h3" className={clases.heading}>
+           {coin?.name}
+        </Typography>
+        <Typography variant="subtitle1" className={clases.description}>
+            {ReactHtmlParser(coin?.description.en.split(".")[0])}
+        </Typography>
+        <div className={clases.marketData}>
+          <span style={{display:"flex"}}>
+            <Typography
+              variant="h5"
+              className={clases.heading}
+            >
+               Rank:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat"
+              }}
+            >
+               {numberWithCommas(coin?.market_cap_rank)}
+            </Typography>
+          </span>
+          <span style={{display:"flex"}}>
+          <Typography
+              variant="h5"
+              className={clases.heading}
+            >
+               Current Price:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat"
+              }}
+            >
+                {symbol}{" "}
+                {numberWithCommas(coin?.market_data_current_price[currency.toLowerCase()]
+                )}
+            </Typography>
+          </span>
+          <span style={{display:"flex"}}>
+          <Typography
+              variant="h5"
+              className={clases.heading}
+            >
+               Market Cap:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat"
+              }}
+            >
+                {symbol}{" "}
+                {numberWithCommas(coin?.market_data_current_cap[currency.toLowerCase()]
+                .toString()
+                .slice(0, -6)
+                )}
+            </Typography>
+          </span>
+        </div>
+      </div>
+      <CoinInfo coin={coin}/>
+    </div>
+);
+};
+
+export default CoinPage;
