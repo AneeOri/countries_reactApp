e2f82b74f2d09c67f4c5a93fef480282978ddbfd
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-03-14 10:27:32 -0600

    [.] Update Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import { AppBar, Container,MenuItem, Select, Toolbar, Typography } from "@mui/material";
+import {createTheme, makeStyles, ThemeProvider} from "@mui/styles";
+import {useHistory} from "react-router-dom";
+import { CryptoState } from "../context/CryptoContext";
+
+
