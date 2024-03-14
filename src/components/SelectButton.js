22cbf768af6e5ac92acb06bc25c99770d1e8c732
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-03-14 10:27:47 -0600

    [.] Update SelectButton Component

--- src/components/SelectButton.js
+++ src/components/SelectButton.js
@@ -1,4 +1,4 @@
-import { makeStyles } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
 const SelectButton = ({children, selected, onClick}) => {
     const useStyles = makeStyles({
